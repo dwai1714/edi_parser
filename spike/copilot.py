741a3c4ad1a0cbc9324ef4fbc5ed36ee90a0fd92
dwai1714 <dc@bwsdev.localdomain>
2023-11-30 11:49:26 +0530

    added spark code

--- spike/copilot.py
+++ spike/copilot.py
@@ -6,9 +6,15 @@ import subprocess
 import time
 import shutil
 
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 def get_root_path():
     return ROOT_DIR
+
 
 def force_async(fn):
     from concurrent.futures import ThreadPoolExecutor
@@ -36,6 +42,7 @@ def execute_transform(input_file_name, output_file_name):
         ).stdout.read()
     )
 
+
 def read_directory(dir_name):
     dir_list = os.listdir(dir_name)
     print(dir_list)
@@ -55,7 +62,8 @@ def finalizer_function():
             out_path = f"{get_root_path()}/outputs/{dir}/"
             if not os.path.exists(out_path):
                 os.makedirs(out_path)
-            execute_transform(input_file,f"{get_root_path()}/outputs/{dir}/{tail}.json")
+            execute_transform(input_file, f"{get_root_path()}/outputs/{dir}/{tail}.json")
+
 
 def validate_if_json(file_name):
     with open(file_name) as json_file:
@@ -65,6 +73,8 @@ def validate_if_json(file_name):
             print("JSON object issue: %s" % e)
             error_tup = (file_name, e)
             return error_tup
+
+
 def list_bad_files(dir_name):
     error_file_list = []
     dir_list = read_directory(dir_name)
@@ -89,8 +99,48 @@ def parse_single_file(input_file_name):
     head, tail = os.path.split(input_file_name)
     execute_transform(input_file_name, f"{get_root_path()}/dc_output/{tail}.json")
 
+
+def find_arrays(input_str):
+    list_of_arrays = []
+
+    line = input_str.replace(':array', '\n:array')
+    line_list = line.split('\n')
+    for i in line_list[:-1]:
+        line_without_del = i.replace(":array<struct<", "")
+        new_list = line_without_del.split(",")
+        array_elem = new_list.pop()
+        list_of_arrays.append(array_elem)
+    print(list_of_arrays)
+
+
+def loop_and_explode(dataframe, col_name):
+    output_df = dataframe.withColumn(col_name, explode(col_name))
+    output_df = output_df.select("*", f"{col_name}.*")
+    output_df = output_df.drop(col_name)
+    return output_df
+
+
+def create_dataframe(spark, path):
+    input_df = spark.read.json(f"{get_root_path()}/{path}")
+    output_df = input_df.withColumn("interchanges", explode("interchanges"))
+    output_df = output_df.select("interchanges.*")
+    output_df.show(1000, False)
+
+    array_list = ['functional_groups', 'transactions', 'segments', 'HL-2000A_loop', 'HL-2000B_loop', 'CLM-2300_loop',  ]
+                  #,'LX-2400_loop' , 'NM1-2310A_loop','SBR-2320_loop', 'NM1-2330A_loop', 'NM1-2330B_loop', 'HL-2000C_loop', 'CLM-2300_loop', 'LX-2400_loop', 'NM1-2310A_loop', 'SBR-2320_loop', 'NM1-2330A_loop', 'NM1-2330B_loop', 'NM1-2010CA_loop', 'NM1-2010BA_loop', 'NM1-2010BB_loop', 'NM1-2010AA_loop', 'NM1-1000A_loop', 'NM1-1000B_loop']
+
+    for array_elem in array_list:
+        output_df = loop_and_explode(output_df, array_elem)
+    output_df = loop_and_explode(output_df, "LX-2400_loop")
+
+    output_df.printSchema()
+    output_df.show(1000, False)
+
+
 if __name__ == "__main__":
     start_time = time.monotonic()
+    # input_str = "[('interchanges', 'array<struct<ISA_01_AuthorizationQualifier:string,ISA_02_AuthorizationInformation:string,ISA_03_SecurityQualifier:string,ISA_04_SecurityInformation:string,ISA_05_SenderQualifier:string,ISA_06_SenderId:string,ISA_07_ReceiverQualifier:string,ISA_08_ReceiverId:string,ISA_09_Date:string,ISA_10_Time:string,ISA_11_RepetitionSeparator:string,ISA_12_Version:string,ISA_13_InterchangeControlNumber:string,ISA_14_AcknowledgmentRequested:string,ISA_15_TestIndicator:string,functional_groups:array<struct<GS_01_FunctionalIdentifierCode:string,GS_02_ApplicationSenderCode:string,GS_03_ApplicationReceiverCode:string,GS_04_Date:string,GS_05_Time:string,GS_06_GroupControlNumber:string,GS_07_ResponsibleAgencyCode:string,GS_08_Version:string,transactions:array<struct<ST_01_TransactionSetIdentifierCode:string,ST_02_TransactionSetControlNumber:string,ST_03_ImplementationConventionReference:string,segments:array<struct<BHT_01:string,BHT_02:string,BHT_03:string,BHT_04:string,BHT_05:string,BHT_06:string,HL-2000A_loop:array<struct<HL-2000B_loop:array<struct<CLM-2300_loop:array<struct<AMT_01:string,AMT_02:string,CL1_01:string,CL1_02:string,CL1_03:string,CLM_01:string,CLM_02:string,CLM_05:struct<CLM_05_01:string,CLM_05_02:string,CLM_05_03:string>,CLM_07:string,CLM_08:string,CLM_09:string,DTP_01:string,DTP_02:string,DTP_03:string,HCP_01:string,HCP_02:string,HCP_04:string,HCP_13:string,HI_01:struct<HI_01_01:string,HI_01_02:string,HI_01_03:string,HI_01_04:string,HI_01_05:string>,HI_02:struct<HI_02_01:string,HI_02_02:string,HI_02_03:string,HI_02_04:string>,HI_03:struct<HI_03_01:string,HI_03_02:string,HI_03_03:string,HI_03_04:string>,HI_04:struct<HI_04_01:string,HI_04_02:string,HI_04_03:string,HI_04_04:string>,LX-2400_loop:array<struct<DTP_01:string,DTP_02:string,DTP_03:string,LX_01:string,SV2_01:string,SV2_02:struct<SV2_02_01:string,SV2_02_02:string>,SV2_03:string,SV2_04:string,SV2_05:string>>,NM1-2310A_loop:array<struct<NM1_01:string,NM1_02:string,NM1_03:string,NM1_04:string,NM1_05:string,NM1_08:string,NM1_09:string,PRV_01:string,PRV_02:string,PRV_03:string,REF_01:string,REF_02:string>>,REF_01:string,REF_02:string,SBR-2320_loop:array<struct<NM1-2330A_loop:array<struct<N3_01:string,N4_01:string,N4_02:string,N4_03:string,NM1_01:string,NM1_02:string,NM1_03:string,NM1_04:string,NM1_05:string,NM1_08:string,NM1_09:string>>,NM1-2330B_loop:array<struct<NM1_01:string,NM1_02:string,NM1_03:string,NM1_08:string,NM1_09:string>>,OI_03:string,OI_06:string,SBR_01:string,SBR_02:string,SBR_03:string,SBR_04:string,SBR_09:string>>>>,HL-2000C_loop:array<struct<CLM-2300_loop:array<struct<AMT_01:string,AMT_02:string,CL1_01:string,CL1_02:string,CL1_03:string,CLM_01:string,CLM_02:string,CLM_05:struct<CLM_05_01:string,CLM_05_02:string,CLM_05_03:string>,CLM_07:string,CLM_08:string,CLM_09:string,DTP_01:string,DTP_02:string,DTP_03:string,HCP_01:string,HCP_02:string,HCP_03:string,HCP_04:string,HI_01:struct<HI_01_01:string,HI_01_02:string,HI_01_03:string,HI_01_04:string>,HI_02:struct<HI_02_01:string,HI_02_02:string>,LX-2400_loop:array<struct<DTP_01:string,DTP_02:string,DTP_03:string,HCP_01:string,HCP_02:string,HCP_03:string,LX_01:string,SV2_01:string,SV2_02:struct<SV2_02_01:string,SV2_02_02:string>,SV2_03:string,SV2_04:string,SV2_05:string>>,NM1-2310A_loop:array<struct<NM1_01:string,NM1_02:string,NM1_03:string,NM1_04:string,NM1_08:string,NM1_09:string>>,REF_01:string,REF_02:string,SBR-2320_loop:array<struct<NM1-2330A_loop:array<struct<NM1_01:string,NM1_02:string,NM1_03:string,NM1_04:string,NM1_08:string,NM1_09:string>>,NM1-2330B_loop:array<struct<NM1_01:string,NM1_02:string,NM1_03:string,NM1_08:string,NM1_09:string>>,OI_03:string,OI_06:string,SBR_01:string,SBR_02:string,SBR_04:string,SBR_09:string>>>>,HL_01:string,HL_02:string,HL_03:string,HL_04:string,NM1-2010CA_loop:array<struct<DMG_01:string,DMG_02:string,DMG_03:string,N3_01:string,N4_01:string,N4_02:string,N4_03:string,NM1_01:string,NM1_02:string,NM1_03:string,NM1_04:string,REF_01:string,REF_02:string>>,PAT_01:string>>,HL_01:string,HL_02:string,HL_03:string,HL_04:string,NM1-2010BA_loop:array<struct<DMG_01:string,DMG_02:string,DMG_03:string,N3_01:string,N4_01:string,N4_02:string,N4_03:string,NM1_01:string,NM1_02:string,NM1_03:string,NM1_04:string,NM1_05:string,NM1_08:string,NM1_09:string>>,NM1-2010BB_loop:array<struct<NM1_01:string,NM1_02:string,NM1_03:string,NM1_08:string,NM1_09:string,REF_01:string,REF_02:string>>,SBR_01:string,SBR_02:string,SBR_03:string,SBR_09:string>>,HL_01:string,HL_03:string,HL_04:string,NM1-2010AA_loop:array<struct<N3_01:string,N4_01:string,N4_02:string,N4_03:string,NM1_01:string,NM1_02:string,NM1_03:string,NM1_08:string,NM1_09:string,REF_01:string,REF_02:string>>,PRV_01:string,PRV_02:string,PRV_03:string>>,NM1-1000A_loop:array<struct<NM1_01:string,NM1_02:string,NM1_03:string,NM1_08:string,NM1_09:string,PER_01:string,PER_02:string,PER_03:string,PER_04:string>>,NM1-1000B_loop:array<struct<NM1_01:string,NM1_02:string,NM1_03:string,NM1_08:string,NM1_09:string>>>>>>>>>>')]"
+    # find_arrays(input_str)
     dir_name = f"{get_root_path()}/outputs/"
     print(
         subprocess.Popen(
@@ -109,19 +159,18 @@ if __name__ == "__main__":
     )
     finalizer_function()
     print(list_bad_files(dir_name))
-    # validate_if_json("/Users/dc/edi_parser/spike/outputs/277/X364-example-two.edi.json")
-    # finalizer_function()
 
-    # spark = (
-    #     SparkSession.builder.appName("redact")
-    #     .master("local[*]")
-    #     .config("spark.sql.codegen.wholeStage", "false")
-    #     .config("spark.driver.memory", "28g")
-    #     .config('spark.jars.packages',
-    #             'org.postgresql:postgresql:42.2.9.jre7')
-    #     .getOrCreate()
-    # )
-    # spark.conf.set("spark.sql.shuffle.partitions", "200")
-    # df = spark.read.json("/Users/dc/edi_parser/spike/outputs/837P")
-    # df.printSchema()
-    # df.select("interchanges").show()
+
+    spark = (
+        SparkSession.builder.appName("edi")
+        .master("local[*]")
+        .config("spark.sql.codegen.wholeStage", "false")
+        .config("spark.executor.memory", "2g")
+        .config("spark.driver.memory", "8g")
+        .config("spark.driver.host", "127.0.0.1")
+        .config("spark.driver.bindAddress", "127.0.0.1")
+        .getOrCreate()
+    )
+    spark.conf.set("spark.sql.shuffle.partitions", "200")
+    path = "outputs/837I"
+    create_dataframe(spark, path)
